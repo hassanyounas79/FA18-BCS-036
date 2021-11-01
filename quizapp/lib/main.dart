018f199c64f1e7a15701449d6ec353b050fd81b2
Hassan Younas <hassanyounas79@gmail.com>
2021-11-01 14:09:20 +0500

    added more features
    Added Drawer, Timer, Random Question Generation, and Result Page.

--- quizapp/lib/main.dart
+++ quizapp/lib/main.dart
@@ -1,135 +1,273 @@
+// ignore_for_file: missing_return
+
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:quizapp/mdlresult.dart';
+import 'package:quizapp/result.dart';
 import 'quiz_brain.dart';
-import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:percent_indicator/percent_indicator.dart';
 
 QuizBrain quizBrain = QuizBrain();
+void main() => runApp(MaterialApp(
+      home: QuizApp(),
+    ));
 
-void main() => runApp(QuizApp());
+class QuizApp extends StatefulWidget {
+  //const QuizApp({ Key? key }) : super(key: key);
 
-class QuizApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.grey.shade900,
-        body: SafeArea(
-          child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: QuizPage(),
-          ),
-        ),
+  _QuizAppState createState() => _QuizAppState();
+}
+
+class _QuizAppState extends State<QuizApp> {
+  List<Icon> scoreKeeper = [];
+  List<MdlResult> record = [];
+  int attempted = 0;
+  getnextQuestion() {
+    setState(() {
+      if (quizBrain.isFinished()) {
+        time.cancel();
+        sec = 5;
+        _dispDialog();
+        quizBrain.reset();
+        scoreKeeper = [];
+      } else {
+        quizBrain.nextQuestion();
+        setState(() {});
+      }
+    });
+  }
+
+  _dispDialog() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (BuildContext context) => Result(record, attempted),
       ),
     );
   }
-}
 
-class QuizPage extends StatefulWidget {
-  @override
-  _QuizPageState createState() => _QuizPageState();
-}
-
-class _QuizPageState extends State<QuizPage> {
-  List<Icon> scoreKeeper = [];
   void checkAnswer(bool userPickedAnswer) {
     bool correctAnswer = quizBrain.getCorrectAnswer();
+    attempted++;
+
     setState(() {
       if (quizBrain.isFinished() == true) {
-        Alert(
-          context: context,
-          title: 'Finshed',
-          desc: 'You\'ve reached the end of the quiz.',
-        ).show();
+        time.cancel();
+        sec = 5;
+        _dispDialog();
+
         quizBrain.reset();
         scoreKeeper = [];
       } else {
         if (userPickedAnswer == correctAnswer) {
+          sec = 5;
           scoreKeeper.add(Icon(
             Icons.check,
             color: Colors.green,
           ));
         } else {
-          scoreKeeper.add(Icon(
-            Icons.close,
-            color: Colors.red,
-          ));
+          record.add(
+              MdlResult(quizBrain.question, userPickedAnswer, correctAnswer));
+          sec = 5;
+          scoreKeeper.add(
+            Icon(
+              Icons.close,
+              color: Colors.red,
+            ),
+          );
         }
         quizBrain.nextQuestion();
       }
     });
   }
 
+  int sec = 5;
+  Timer time;
+  bool first = true;
+  _startTimer() {
+    time = Timer.periodic(
+      Duration(seconds: 1),
+      (Timer t) {
+        setState(() {
+          if (this.sec > 0) {
+            this.sec = this.sec - 1;
+          } else {
+            this.sec = 5;
+            getnextQuestion();
+            setState(() {});
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Expanded(
-          flex: 6,
-          child: Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Center(
-              child: Text(
-                quizBrain.getQuestionText(),
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                  fontSize: 25.0,
-                  color: Colors.white,
+    if (first) {
+      first = false;
+      _startTimer();
+    }
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        onDrawerChanged: (t) {},
+        appBar: AppBar(
+          title: Text("Quiz App"),
+          bottomOpacity: 0,
+          backgroundColor: Colors.grey.shade400,
+          toolbarOpacity: 0.9,
+        ),
+        drawer: Drawer(
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundColor: Colors.amber[400],
+                    ),
+                    SizedBox(
+                      height: 5,
+                    ),
+                    Text(
+                      "Quiz App",
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  ],
+                ),
+
+                // decoration: BoxDecoration(color: Colors.black54),
+              ),
+              Divider(
+                thickness: 3,
+                indent: 40,
+                endIndent: 40,
+                color: Colors.blueGrey.shade600,
+              ),
+              ListTile(
+                subtitle: Text(
+                  "Total Questions: 10",
+                  style: TextStyle(fontSize: 20),
                 ),
               ),
+              ListTile(
+                subtitle: Text(
+                  "Remaining Questions: ${10 - attempted}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              ListTile(
+                subtitle: Text(
+                  "Total Correct Answers:${attempted - record.length}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+              ListTile(
+                subtitle: Text(
+                  "Total Wrong Answers: ${record.length}",
+                  style: TextStyle(fontSize: 20),
+                ),
+              ),
+            ],
+          ),
+        ),
+        backgroundColor: Colors.grey.shade900,
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(50),
+                  child: CircularPercentIndicator(
+                    // animation: true,
+                    radius: 100.0,
+                    lineWidth: 7.0,
+                    percent: (sec / 10) * 2,
+                    linearGradient: LinearGradient(
+                        colors: [Colors.blue.shade900, Colors.red]),
+
+                    center: new Text(
+                      sec.toString(),
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    //progressColor: Colors.green,
+                  ),
+                ),
+                Expanded(
+                  flex: 4,
+                  child: Padding(
+                    padding: EdgeInsets.all(10.0),
+                    child: Center(
+                      child: Text(
+                        quizBrain.getQuestionText(),
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 25.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    height: 50,
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      textColor: Colors.white,
+                      color: Colors.green,
+                      child: Text(
+                        'True',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20.0,
+                        ),
+                      ),
+                      onPressed: () {
+                        checkAnswer(true);
+                        //The user picked true.
+                      },
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.all(15.0),
+                    child: FlatButton(
+                      color: Colors.red,
+                      child: Text(
+                        'False',
+                        style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.white,
+                        ),
+                      ),
+                      onPressed: () {
+                        checkAnswer(false);
+                        //The user picked false.
+                      },
+                    ),
+                  ),
+                ),
+                Row(
+                  children: scoreKeeper,
+                ),
+              ],
             ),
           ),
         ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              textColor: Colors.white,
-              color: Colors.green,
-              child: Text(
-                'True',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
-              onPressed: () {
-                checkAnswer(true);
-                //The user picked true.
-              },
-            ),
-          ),
-        ),
-        Expanded(
-          child: Padding(
-            padding: EdgeInsets.all(15.0),
-            child: FlatButton(
-              color: Colors.red,
-              child: Text(
-                'False',
-                style: TextStyle(
-                  fontSize: 20.0,
-                  color: Colors.white,
-                ),
-              ),
-              onPressed: () {
-                checkAnswer(false);
-                //The user picked false.
-              },
-            ),
-          ),
-        ),
-        Row(
-          children: scoreKeeper,
-        ),
-      ],
+      ),
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
--- quizapp/lib/quiz_brain.dart
+++ quizapp/lib/quiz_brain.dart
@@ -1,9 +1,13 @@
+import 'dart:math';
 import 'package:quizapp/question.dart';
 
 class QuizBrain {
   int _questionNumber = 0;
+  int question = 1;
 
-  List<Question> _questionBank = [
+  List<int> atmpq = [Random().nextInt(12)];
+
+  List<Question> questionBank = [
     Question('Some cats are actually allergic to humans', true),
     Question('You can lead a cow down stairs but not up stairs.', false),
     Question('Approximately one quarter of human bones are in the feet.', true),
@@ -32,21 +36,27 @@ class QuizBrain {
   ];
   //Create Next Question Function
   void nextQuestion() {
-    if (_questionNumber < _questionBank.length - 1) {
-      _questionNumber++;
+    question++;
+    int no = Random().nextInt(13);
+    while (atmpq.contains(no)) {
+      no = Random().nextInt(13);
     }
+    if (question <= questionBank.length) {
+      _questionNumber = no;
+    }
+    atmpq.add(no);
   }
 
   String getQuestionText() {
-    return _questionBank[_questionNumber].questionText;
+    return questionBank[_questionNumber].questionText;
   }
 
   bool getCorrectAnswer() {
-    return _questionBank[_questionNumber].questionAnswer;
+    return questionBank[_questionNumber].questionAnswer;
   }
 
   bool isFinished() {
-    if (_questionNumber >= _questionBank.length - 1) {
+    if (this.question >= 10) {
       return true;
     } else {
       return false;
@@ -54,27 +64,8 @@ class QuizBrain {
   }
 
   void reset() {
+    atmpq = [Random().nextInt(12)];
+    question = 0;
     _questionNumber = 0;
   }
 }
-
-//  void nextQuestion() {
-//    if (_questionNumber < _questionBank.length - 1) {
-//      _questionNumber++;
-//    }
-//  }
-//
-//  String getQuestionText() {
-//    return _questionBank[_questionNumber].questionText;
-//  }
-//
-//  bool getCorrectAnswer() {
-//    return _questionBank[_questionNumber].questionAnswer;
-//  }
-
-//TODO: Step 3 Part A - Create a method called isFinished() here that checks to see if we have reached the last question. It should return (have an output) true if we've reached the last question and it should return false if we're not there yet.
-
-//TODO: Step 3 Part B - Use a print statement to check that isFinished is returning true when you are indeed at the end of the quiz and when a restart should happen.
-
-//TODO: Step 4 Part B - Create a reset() method here that sets the questionNumber back to 0.
-//}
